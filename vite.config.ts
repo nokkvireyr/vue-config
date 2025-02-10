756bc8e36897f8396756f736f16bf08d991921b3
Nökkvi Reyr Guðfinnsson <nokkvi@nokkvi.is>
2025-02-10 23:10:53 +0000

    Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from './package.json' with { type: 'json' };
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
