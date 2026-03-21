b25a881a9e547dc6ef98e80c9d1ee917c1012afd
Nökkvi Reyr Guðfinnsson <nokkvi@nokkvi.is>
2026-03-21 23:24:53 +0000

    feat(lint): Updated lint config to allow console.warn and error fix(types): Export oxlint and oxfmt config types

--- src/oxfmt/oxfmt.ts
+++ src/oxfmt/oxfmt.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'oxfmt';
+import type { defineConfig } from 'oxfmt';
+export type { OxfmtConfig } from 'oxfmt';
+
 type OxfmtConfig = Parameters<typeof defineConfig>[0];
 export const oxfmtConfig = (overrides: OxfmtConfig = {}): OxfmtConfig => ({
   semi: true,
